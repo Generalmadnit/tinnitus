23321c3d6248bab42009f82648a80b7457570e9f
generalmadnit <pathurinitin@gmail.com>
2023-12-15 12:15:32 +0530

    changed main

--- amplifier/lib/main.dart
+++ amplifier/lib/main.dart
@@ -1,228 +1,125 @@
-// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors
-
-import 'dart:async';
-import 'package:audio_session/audio_session.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter/material.dart';
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, unused_import
+import 'dart:math';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/material.dart';
 
-typedef _Fn = void Function();
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 
-const theSource = AudioSource.microphone;
-
-class SimpleRecorder extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _SimpleRecorderState createState() => _SimpleRecorderState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _SimpleRecorderState extends State<SimpleRecorder> {
-  Codec _codec = Codec.aacMP4;
-  String _mPath = 'tau_file.mp4';
-  FlutterSoundPlayer? _mPlayer = FlutterSoundPlayer();
-  FlutterSoundRecorder? _mRecorder = FlutterSoundRecorder();
-  bool _mPlayerIsInited = false;
-  bool _mRecorderIsInited = false;
-  bool _mplaybackReady = false;
-
+class _MyAppState extends State {
+  // Define parameters
+  double amplitude = 50.0;
+  double frequency = 20.0;
+  double phaseShift = 0.0;
+  int numSamples = 100;
+  List _data = [];
   @override
   void initState() {
-    _mPlayer!.openPlayer().then((value) {
-      setState(() {
-        _mPlayerIsInited = true;
-      });
-    });
-
-    openTheRecorder().then((value) {
-      setState(() {
-        _mRecorderIsInited = true;
-      });
-    });
     super.initState();
+    _generateSineWaveData();
   }
 
-  @override
-  void dispose() {
-    _mPlayer!.closePlayer();
-    _mPlayer = null;
-
-    _mRecorder!.closeRecorder();
-    _mRecorder = null;
-    super.dispose();
-  }
-
-  Future<void> openTheRecorder() async {
-    if (!kIsWeb) {
-      var status = await Permission.microphone.request();
-      if (status != PermissionStatus.granted) {
-        throw RecordingPermissionException('Microphone permission not granted');
-      }
+  void _generateSineWaveData() {
+    _data = [];
+    for (int i = 0; i < numSamples; i++) {
+      double x = 2 * pi * i / numSamples;
+      double y = amplitude * sin(2 * pi * frequency * x + phaseShift);
+      _data.add(y);
     }
-    await _mRecorder!.openRecorder();
-    if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
-      _codec = Codec.opusWebM;
-      _mPath = 'tau_file.webm';
-      if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
-        _mRecorderIsInited = true;
-        return;
-      }
-    }
-    final session = await AudioSession.instance;
-    await session.configure(AudioSessionConfiguration(
-      avAudioSessionCategory: AVAudioSessionCategory.playAndRecord,
-      avAudioSessionCategoryOptions:
-          AVAudioSessionCategoryOptions.allowBluetooth |
-              AVAudioSessionCategoryOptions.defaultToSpeaker,
-      avAudioSessionMode: AVAudioSessionMode.spokenAudio,
-      avAudioSessionRouteSharingPolicy:
-          AVAudioSessionRouteSharingPolicy.defaultPolicy,
-      avAudioSessionSetActiveOptions: AVAudioSessionSetActiveOptions.none,
-      androidAudioAttributes: const AndroidAudioAttributes(
-        contentType: AndroidAudioContentType.speech,
-        flags: AndroidAudioFlags.none,
-        usage: AndroidAudioUsage.voiceCommunication,
-      ),
-      androidAudioFocusGainType: AndroidAudioFocusGainType.gain,
-      androidWillPauseWhenDucked: true,
-    ));
-
-    _mRecorderIsInited = true;
-  }
-
-  // ----------------------  Here is the code for recording and playback -------
-
-  void record() {
-    _mRecorder!
-        .startRecorder(
-      toFile: _mPath,
-      codec: _codec,
-      audioSource: theSource,
-    )
-        .then((value) {
-      setState(() {});
-    });
-  }
-
-  void stopRecorder() async {
-    await _mRecorder!.stopRecorder().then((value) {
-      setState(() {
-        //var url = value;
-        _mplaybackReady = true;
-      });
-    });
-  }
-
-  void play() {
-    assert(_mPlayerIsInited &&
-        _mplaybackReady &&
-        _mRecorder!.isStopped &&
-        _mPlayer!.isStopped);
-    _mPlayer!
-        .startPlayer(
-            fromURI: _mPath,
-            //codec: kIsWeb ? Codec.opusWebM : Codec.aacADTS,
-            whenFinished: () {
-              setState(() {});
-            })
-        .then((value) {
-      setState(() {});
-    });
-  }
-
-  void stopPlayer() {
-    _mPlayer!.stopPlayer().then((value) {
-      setState(() {});
-    });
-  }
-
-// ----------------------------- UI --------------------------------------------
-
-  _Fn? getRecorderFn() {
-    if (!_mRecorderIsInited || !_mPlayer!.isStopped) {
-      return null;
-    }
-    return _mRecorder!.isStopped ? record : stopRecorder;
-  }
-
-  _Fn? getPlaybackFn() {
-    if (!_mPlayerIsInited || !_mplaybackReady || !_mRecorder!.isStopped) {
-      return null;
-    }
-    return _mPlayer!.isStopped ? play : stopPlayer;
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget makeBody() {
-      return Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(3),
-            padding: const EdgeInsets.all(3),
-            height: 80,
-            width: double.infinity,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: const Color(0xFFFAF0E6),
-              border: Border.all(
-                color: Colors.indigo,
-                width: 3,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sine Wave Generator'),
+        ),
+        body: Column(
+          children: [
+            // Parameters sliders
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Amplitude: ${amplitude.toStringAsFixed(2)}'),
+                Slider(
+                  value: amplitude,
+                  min: 0.0,
+                  max: 100.0,
+                  onChanged: (value) => setState(() => amplitude = value),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Frequency: ${frequency.toStringAsFixed(2)} Hz'),
+                Slider(
+                  value: frequency,
+                  min: 0.0,
+                  max: 20000.0,
+                  onChanged: (value) => setState(() => frequency = value),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Phase Shift: ${phaseShift.toStringAsFixed(2)} π'),
+                Slider(
+                  value: phaseShift,
+                  min: 0.0,
+                  max: 2 * pi,
+                  onChanged: (value) => setState(() => phaseShift = value),
+                ),
+              ],
+            ),
+            // Generate button
+            ElevatedButton(
+              child: const Text('Generate'),
+              onPressed: () => setState(() => _generateSineWaveData()),
+            ), // Sine wave graph
+            Expanded(
+              child: Container(
+                color: Colors.grey[200],
+                child: CustomPaint(
+                  painter: SineWavePainter(_data),
+                  size: Size.infinite,
+                ),
               ),
             ),
-            child: Row(children: [
-              ElevatedButton(
-                onPressed: getRecorderFn(),
-                //color: Colors.white,
-                //disabledColor: Colors.grey,
-                child: Text(_mRecorder!.isRecording ? 'Stop' : 'Record'),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Text(_mRecorder!.isRecording
-                  ? 'Recording in progress'
-                  : 'Recorder is stopped'),
-            ]),
-          ),
-          Container(
-            margin: const EdgeInsets.all(3),
-            padding: const EdgeInsets.all(3),
-            height: 80,
-            width: double.infinity,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: const Color(0xFFFAF0E6),
-              border: Border.all(
-                color: Colors.indigo,
-                width: 3,
-              ),
-            ),
-            child: Row(children: [
-              ElevatedButton(
-                onPressed: getPlaybackFn(),
-                //color: Colors.white,
-                //disabledColor: Colors.grey,
-                child: Text(_mPlayer!.isPlaying ? 'Stop' : 'Play'),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Text(_mPlayer!.isPlaying
-                  ? 'Playback in progress'
-                  : 'Player is stopped'),
-            ]),
-          ),
-        ],
-      );
-    }
-
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: const Text('Simple Recorder'),
+          ],
+        ),
       ),
-      body: makeBody(),
     );
   }
+}
+
+class SineWavePainter extends CustomPainter {
+  final List data;
+  SineWavePainter(this.data);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 2.0;
+    final path = Path();
+    final centerY = size.height / 2.0;
+    path.moveTo(0.0, centerY + data[0]);
+    for (int i = 1; i < data.length; i++) {
+      double x = size.width * i / (data.length - 1);
+      double y = centerY + data[i];
+      path.lineTo(x, y);
+    }
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
